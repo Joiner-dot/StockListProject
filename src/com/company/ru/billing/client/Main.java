50a0d00c8b62f1a6e8a18c1cb953ac38eec1c09c
George <egor_kuzmin22@mail.ru>
2020-03-17 18:01:14 +0300

    FIleLoader

--- src/com/company/ru/billing/client/Main.java
+++ src/com/company/ru/billing/client/Main.java
@@ -13,35 +13,7 @@ import java.util.Date;
 public class Main{
     //CatalogFileLoader для 6 лабораторной
     public static void main (String[] args) throws IOException{
-        String line = "Конфеты ’Маска’;45;120";
-        String[] valuelist = line.split (";");
-        FoodItem foodItem = new FoodItem (valuelist[0], Float.parseFloat (valuelist[1]), Short.parseShort (valuelist[2]));
-        foodItem.printAll ();
         ItemCatalog itemCatalog = new ItemCatalog ();
-        try{
-            itemCatalog.addItem (new GenericItem ("КОркунов", 10f, Category.GENERAL));
-        } catch (ItemAlreadyExistsException e){
-            e.printStackTrace ();
-        }
-
-        long begin = new Date ().getTime ();
-        for (int i = 0; i < 100000; i++)
-            itemCatalog.findItemByID (5);
-        long end = new Date ().getTime ();
-        System.out.println ("HashMap Time = " + (int) (end - begin));
-        begin = new Date ().getTime ();
-        for (int i = 0; i < 100000; i++)
-            itemCatalog.findItemByIDAL (5);
-        end = new Date ().getTime ();
-        System.out.println ("ArrayList Time = " + (int) (end - begin));
-
-        CatalogLoader catalogLoader = new SubLoader ();
-        try{
-            catalogLoader.load (itemCatalog);
-        } catch (CatalogLoadExceptions e){
-            System.out.println (e.getName ());
-            e.printStackTrace ();
-        }
         CatalogFileLoader file = new CatalogFileLoader ("/Users/egor_kuzmin/IdeaProjects/StockListProject/src/com/company/Readme.txt");
         try{
             file.load (itemCatalog);
