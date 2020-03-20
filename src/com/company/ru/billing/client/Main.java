04b30071467685007101e8ee3d096e117289af14
George <egor_kuzmin22@mail.ru>
2020-03-20 22:49:48 +0300

    StockListProject

--- src/com/company/ru/billing/client/Main.java
+++ src/com/company/ru/billing/client/Main.java
@@ -6,6 +6,7 @@ import com.company.ru.billing.stocklist.GenericItem.Category;
 import com.company.ru.billing.stocklist.ItemCatalog;
 import com.company.ru.itmo.exceptions.CatalogLoadExceptions;
 import com.company.ru.itmo.exceptions.ItemAlreadyExistsException;
+
 import java.io.IOException;
 import java.util.Date;
 
@@ -14,14 +15,19 @@ import static com.company.ru.billing.stocklist.GenericItem.Category.*;
 public class Main{
     //!!! CatalogFileLoader, ItemCatalog, CatalogLoadException,SubLoader для 6 лабораторной
     public static void main (String[] args) throws IOException{
+        SubLoader subLoader = new SubLoader ();
         ItemCatalog itemCatalog = new ItemCatalog ();
+        try{
+            subLoader.load (itemCatalog);
+        } catch (CatalogLoadExceptions catalogLoadExceptions){
+            catalogLoadExceptions.printStackTrace ();
+        }
         CatalogFileLoader file = new CatalogFileLoader ("src/com/company/Readme.txt");
         try{
-            file.load (itemCatalog);
+            file.windows1251load (itemCatalog);
         } catch (CatalogLoadExceptions | IOException catalogLoadExceptions){
             catalogLoadExceptions.printStackTrace ();
         }
-
         itemCatalog.printItem ();
 
     }
